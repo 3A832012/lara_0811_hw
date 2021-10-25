8f4124d0b83bc63a953fc6ad8d868a15eec769b7
SuYiXuan <3a832012@gm.student.ncut.edu.tw>
2021-10-25 23:08:31 +0800

    修正

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,8 @@ use App\Http\Controllers\NewsController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('home',[HomeController::class,'index'])->name('home.index');
-Route::get('about',[HomeController::class,'index'])->name('about.index');
-Route::get('news',[HomeController::class,'index'])->name('news.index');
+
+Route::get('/',[HomeController::class,'index'])->name('home.index');
+Route::get('about',[AboutController::class,'index'])->name('about.index');
+Route::get('news',[NewsController::class,'index'])->name('news.index');
